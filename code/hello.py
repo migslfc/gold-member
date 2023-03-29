fcecd9dc35ccf33b33843abd66b74f77a80cc366
Miguel Camacho <mcamacho0808@gmail.com>
2023-03-29 00:41:22 +0000

    acg-hello

---
+++ code/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+
+#this is a full line comment
+
+print ("hello, young world!") # this is a training comment
+
+"""
+This i not a block comment
+This is a multiline string.
+"""
+
